26f8f70674a542c7f14d09e3d0eb17f5b12e618e
Nicholas Walter Knize <nknize@apache.org>
2024-03-21 08:18:01 -0500

    update script to remove old package before publishing new
    This is attempt 1 at updating the gha script to remove old opensearch
    packages before uploading the new one.
    
    Signed-off-by: Nicholas Walter Knize <nknize@apache.org>

--- scripts/gradle/publish-to-github-packages.sh
+++ scripts/gradle/publish-to-github-packages.sh
@@ -44,6 +44,15 @@ for pom in $(find "$basedir" \( -name '.nexus' \) -prune -false -o -name '*.pom'
 	sources="$dir/$artifact-$version-sources.jar"
 	javadoc="$dir/$artifact-$version-javadoc.jar"
 	pomfile="$dir/$artifact-$version.pom"
+	token=$(env.ACCESS_TOKEN)
+
+	echo "removing old package: $artifact"
+
+	rmcommand="curl -L -X DELETE -H \"Accept: application/vnd.github+json\" \
+	        -H \"Authorization: Bearer $token\" -H \"X-GitHub-Api-Version: 2022-11-28\" \
+	        https://api.github.com/$owner/maven/$artifact"
+
+	eval $rmcommand	
 
 	command="mvn -e -q org.apache.maven.plugins:maven-deploy-plugin:2.4:deploy-file \
 		-DrepositoryId=$serverid \
